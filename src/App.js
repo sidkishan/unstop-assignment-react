b798043b86f65e0196b74fdeede5af21c900f46f
Siddharth Kishan <kishan.siddharth2939@gmail.com>
2024-01-07 11:40:11 +0530

    responsiveness

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { useMediaQuery } from "react-responsive";
 import "./App.css";
 import DesktopUi from "./Components/Desktop/DesktopUi";
 import "./Components/Desktop/styles.css";
@@ -7,10 +8,12 @@ import MobileUI from "./Components/Mobile/MobileUI";
 
 const App = () => {
   const [isMobile, setIsMobile] = useState(false);
+  const isDesktop = useMediaQuery({ query: "(min-width:1224px)" });
+  const isMobileDevice = useMediaQuery({ query: "(max-width: 1224px)" });
   return (
     <div className="App">
       <AppContext.Provider value={{ isMobile, setIsMobile }}>
-        {isMobile ? <MobileUI /> : <DesktopUi />}
+        {isMobileDevice || isMobile ? <MobileUI /> : <DesktopUi />}
       </AppContext.Provider>
     </div>
   );
